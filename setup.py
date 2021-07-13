6cd4504a4f9d1c17fa036f999ccfa6c46abd8c1c
Himmalay Devulapalli <65706245+Himmalay-Devulapalli@users.noreply.github.com>
2021-07-13 14:23:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,5 @@ from cx_Freeze import setup,Executable
 setup(name ="startup",
            version ="0.1",
            description = " ",
-           executables=[Executable("startup_virus.py")])
-
-
-
-#navigate to dir of the location containing this file and py source file and run the command ->'python setup.py build'
-#both setup.py and py source files should be in one folder 
+           executables=[Executable("startup_virus.py")]
+     )
